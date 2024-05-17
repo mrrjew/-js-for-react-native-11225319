4fd0a7c4776b57f0cd86233dbcce5248daeb1c50
mrrjew <jwlarbi15@gmail.com>
2024-05-17 11:24:12 -0700

    feat(task 1): Implemented array manipulation functionality

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,28 +8,9 @@ function processArray(numbers) {
       }
     });
   }
-  
-  // Example for Task 1 of a new array where each even number is squared and each odd number is trippled 
 
   const numbers = [1, 2, 3, 4, 5];
   const processedNumbers = processArray(numbers);
   console.log('Processed numbers:', processedNumbers); 
-  // Output: [3, 4, 9, 16, 15]
 
 
-  
-   // Task 2: formatArrayStrings function
-   function formatArrayStrings(strings, processedNumbers) {
-    return strings.map((str, index) => {
-      if (processedNumbers[index] % 2 === 0) {
-        return str.toUpperCase();
-      } else {
-        return str.toLowerCase();
-      }
-    });
-  }
-  
-  // Example for Task 2
-  const names = ['Mary', 'Esinam' , 'Kate'];
-  const modifiedNames = formatArrayStrings(names, processedNumbers);
-  console.log('Modified names:', modifiedNames); // Output: ['MARY', 'ESINAM', 'kate']
