b27c9d2505757606f46d6a4b262a892e709711e0
mrrjew <jwlarbi15@gmail.com>
2024-05-17 11:25:22 -0700

    feat(task 3): Implemented user profiles functionality

---
+++ userInfo.js
@@ -0,0 +1,14 @@
+// // Task 3: createUserProfiles function
+// function createUserProfiles(names, modifiedNames) {
+//     return names.map((name, index) => ({
+//       originalName: name,
+//       modifiedName: modifiedNames[index],
+//       id: index + 1
+//     }));
+//   }
+
+
+  
+//   // Example for  Task 3 above
+//   const userProfiles = createUserProfiles(names, modifiedNames);
+//   console.log('User profiles:', userProfiles);
